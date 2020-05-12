3b61a0c6bbf1bcbefcb00306ba6088029b9af0d2
zhanghaoran <240964503@qq.com>
2020-05-12 18:57:41 +0800

    1、我的设备列表完成 2、设备详情结构初步完成 

---
+++ pages/myDevice/deviceInfo.vue
@@ -0,0 +1,17 @@
+<template>
+	<view>设备详情</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	},
+	onLoad(option) {
+		console.log(option, 1111111111111); //打印出上个页面传递的参数。
+	},
+	methods: {}
+};
+</script>
+
+<style scoped lang="less"></style>
