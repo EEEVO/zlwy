6cdc73c4cc912c33301b2fe201c4947d957bf137
zhanghaoran <240964503@qq.com>
2020-05-14 20:20:17 +0800

    添加了过渡动画

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App'
 import uView from "./uview-ui/index.js";
+import ourLoading from '@/components/our-loading/our-loading.vue'
+Vue.component('ourLoading', ourLoading)
 
 Vue.use(uView);
 
@@ -9,6 +11,6 @@ Vue.config.productionTip = false
 App.mpType = 'app'
 
 const app = new Vue({
-  ...App
+	...App
 })
 app.$mount()
