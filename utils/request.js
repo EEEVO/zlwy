a41cb77fc75798aaf26fc4ee8aec8a7aa5afb2ee
zhanghaoran <240964503@qq.com>
2020-05-14 20:10:36 +0800

    修复换设备的情况下的登录鉴权问题

--- utils/request.js
+++ utils/request.js
@@ -8,12 +8,8 @@ const http = new Request();
 
 http.setConfig((config) => {
 	config.baseUrl = 'https://zinlin.cn/zleq/';
-	config.header = {
-		Authorization: getUserToken() // 用户身份标识
-	}
 	return config
 })
-
 
 http.validateStatus = (statusCode) => {
 	return statusCode && statusCode >= 200 && statusCode < 300
@@ -21,26 +17,28 @@ http.validateStatus = (statusCode) => {
 
 http.interceptor.request((config, cancel) => {
 	/* cancel 为函数，如果调用会取消本次请求。需要注意：调用cancel,本次请求的catch仍会执行。必须return config */
+	console.log("getUserToken()", getUserToken())
 	config.header = {
 		...config.header,
-		a: 1 // 演示拦截器header加参
+		Authorization: getUserToken()
 	}
 	// 演示custom 用处
 	// if (config.custom.auth) {
 	//   config.header.token = 'token'
 	// }
-	/**
-	/* 演示cancel 函数
-	if (!token) { // 如果token不存在，调用cancel 会取消本次请求，不会进入响应拦截器，但是该函数的catch() 仍会执行
-	  cancel('token 不存在', config) //  把修改后的config传入，之后响应就可以拿到修改后的config。 如果调用了cancel但是不传修改后的config，则catch((err) => {}) err.config 为request拦截器修改之前的config
-	}
-	**/
+
+	// 如果token不存在，调用cancel 会取消本次请求，不会进入响应拦截器，但是该函数的catch() 仍会执行
+	// if (!getUserToken()) {
+	// 	cancel('token 不存在', config) //  把修改后的config传入，之后响应就可以拿到修改后的config。 如果调用了cancel但是不传修改后的config，则catch((err) => {}) err.config 为request拦截器修改之前的config
+	// 	removeUserToken()
+	// }
 	return config
 })
 
 http.interceptor.response((response) => {
 	/* 对响应成功做点什么 （statusCode === 200），必须return response*/
-	if (response.data.code === 10004) { // 服务端返回的状态码不等于200，则reject()
+	// 服务端返回的状态码不等于200，则reject()
+	if (response.data.code === 10004) {
 		removeUserToken()
 	}
 	return response.data
