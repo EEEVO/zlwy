fefc650bc49882f6bd57938e4b82b130f54fc2cf
zhangqinghe <516567166@qq.com>
2020-07-31 18:54:22 +0800

    增加模型数据选择

--- api/device.js
+++ api/device.js
@@ -5,9 +5,9 @@ import http from '@/utils/request.js'
  * @param {string}
  */
 export const newBind = (device_id) => {
-  return http.post('v1/device/newBind', {
-    device_id
-  })
+	return http.post('v1/device/newBind', {
+		device_id
+	})
 }
 
 /**
@@ -15,11 +15,11 @@ export const newBind = (device_id) => {
  * @param {string} admin 0-全部设备 1-我管理的设备
  */
 export const listDevices = (admin) => {
-  return http.get('v1/mine/listDevices', {
-    params: {
-      admin
-    }
-  })
+	return http.get('v1/mine/listDevices', {
+		params: {
+			admin
+		}
+	})
 }
 
 /**
@@ -27,9 +27,9 @@ export const listDevices = (admin) => {
  * @param {string}
  */
 export const delDevice = (device_id) => {
-  return http.post('/v1/device/delDevice', {
-    device_id
-  })
+	return http.post('/v1/device/delDevice', {
+		device_id
+	})
 }
 
 /**
@@ -37,11 +37,11 @@ export const delDevice = (device_id) => {
  * @param {string} device_id 设备ID
  */
 export const deviceDetail = (device_id) => {
-  return http.get('v1/mine/deviceDetail', {
-    params: {
-      device_id
-    }
-  })
+	return http.get('v1/mine/deviceDetail', {
+		params: {
+			device_id
+		}
+	})
 }
 
 /**
@@ -49,11 +49,11 @@ export const deviceDetail = (device_id) => {
  * @param {string} device_id 设备ID
  */
 export const paramList = (device_id) => {
-  return http.get('v1/mine/paramList', {
-    params: {
-      device_id
-    }
-  })
+	return http.get('v1/mine/paramList', {
+		params: {
+			device_id
+		}
+	})
 }
 
 /**
@@ -61,11 +61,11 @@ export const paramList = (device_id) => {
  * @param {string} device_id 设备ID
  */
 export const historyCond = (device_id) => {
-  return http.get('v1/mine/historyCond', {
-    params: {
-      device_id
-    }
-  })
+	return http.get('v1/mine/historyCond', {
+		params: {
+			device_id
+		}
+	})
 }
 
 /**
@@ -73,14 +73,14 @@ export const historyCond = (device_id) => {
  * @param {string} device_id 设备ID
  */
 export const historyQuery = (device_id, duration, endTime, paramCode) => {
-  return http.get('v1/mine/historyQuery', {
-    params: {
-      device_id,
-      duration,
-      endTime,
-      paramCode
-    }
-  })
+	return http.get('v1/mine/historyQuery', {
+		params: {
+			device_id,
+			duration,
+			endTime,
+			paramCode
+		}
+	})
 }
 
 /**
@@ -88,8 +88,19 @@ export const historyQuery = (device_id, duration, endTime, paramCode) => {
  * @param {string} device_id 设备ID
  */
 export const modifyBind = (device_id, uids) => {
-  return http.post('v1/device/modifyBind', {
-    device_id,
-    uids
-  })
+	return http.post('v1/device/modifyBind', {
+		device_id,
+		uids
+	})
+}
+
+/**
+ * 获取数据模型明细
+ */
+export const modeDetail = (modeId) => {
+	return http.get('v1/device/modeDetail', {
+		params: {
+			modeId
+		}
+	})
 }
--- pages/myDevice/deviceInfo.vue
+++ pages/myDevice/deviceInfo.vue
@@ -1,138 +1,174 @@
 <template>
-  <view class="main">
-    <view class="data-content">
-      <u-cell-group>
-        <u-cell-item :arrow="false" :title="deviceName" :center="true" :title-style="titleStyle" @click="update">
-          <view slot="right-icon" class="title">
-            <view slot="label">数据质量：<u-tag :text="dataQuality" mode="dark" :type="type" /><u-icon size="40" name="reload"></u-icon></view>
-          </view>
-        </u-cell-item>
-        <u-cell-item v-for="(item, index) of dataList" :key="index" :value="item.param_value" :center="true" 
-			:arrow="true" :use-label-slot="true" :title-style="{'color':'#424242'}">
-		  <view slot="title">{{ item.name }}</view>
-          <view slot="label">{{ item.time }}</view>
-        </u-cell-item>
-      </u-cell-group>
-    </view>
-    <view class="btn-content" v-if="admin === '1'"><u-button type="success" :custom-style="customStyle" @click="managPersonnel">管理绑定人员</u-button></view>
-    <view class="btn-content" v-if="history_flag === '2'"><u-button type="primary" :custom-style="customStyle" @click="goHistoryData">历史数据</u-button></view>
-    <view class="btn-content" v-if="history_flag === '1' && admin === '1'"><u-button type="primary" :custom-style="customStyle" @click="goHistoryData">历史数据</u-button></view>
-    <ourLoading isFullScreen :active="httpStatus" text="loading..." />
-  </view>
+	<view class="main">
+		<view class="data-content">
+			<u-cell-group>
+				<u-cell-item :arrow="false" :center="true" :title-style="titleStyle" @click="update">
+					<view slot="title">{{ deviceName }}
+						<u-icon size="40" style="margin-left: 30upx;" name="reload"></u-icon>
+					</view>
+					<view slot="right-icon" class="title">
+						<view slot="label">数据质量：
+							<u-tag :text="dataQuality" mode="dark" :type="type" />
+						</view>
+					</view>
+				</u-cell-item>
+				<u-cell-item @click="openModify" :index="index" v-for="(item, index) of dataList" :value="item.param_value" :center="true"
+				 :arrow="item.modify_flag == 0" :key="index" :title-style="{'color':'#424242'}" :icon-size="100">
+					<view slot="title">{{ item.name }}</view>
+					<view slot="label">{{ item.time }}</view>
+				</u-cell-item>
+			</u-cell-group>
+		</view>
+		<view class="btn-content" v-if="admin === '1'">
+			<u-button type="success" :custom-style="customStyle" @click="managPersonnel">管理绑定人员</u-button>
+		</view>
+		<view class="btn-content" v-if="history_flag === '2'">
+			<u-button type="primary" :custom-style="customStyle" @click="goHistoryData">历史数据</u-button>
+		</view>
+		<view class="btn-content" v-if="history_flag === '1' && admin === '1'">
+			<u-button type="primary" :custom-style="customStyle" @click="goHistoryData">历史数据</u-button>
+		</view>
+		<ourLoading isFullScreen :active="httpStatus" text="loading..." />
+		<u-action-sheet :list="modeList" @click="actionClick" v-model="modifyShow"></u-action-sheet>
+	</view>
 </template>
 
 <script>
-import { deviceDetail, paramList } from '@/api/device.js';
-export default {
-  data() {
-    return {
-      setIntervalObj: null,
-      customStyle: {
-        marginBottom: '10px'
-      },
-      titleStyle: {
-        fontSize: '16px'
-      },
-      httpStatus: true,
-      deviceId: '',
-      dataQuality: '差',
-      dataList: [],
+	import {
+		deviceDetail,
+		paramList,
+		modeDetail
+	} from '@/api/device.js';
+	export default {
+		data() {
+			return {
+				setIntervalObj: null,
+				customStyle: {
+					marginBottom: '10px'
+				},
+				titleStyle: {
+					fontSize: '16px'
+				},
+				httpStatus: true,
+				deviceId: '',
+				dataQuality: '差',
+				dataList: [],
 
-	  deviceName: '',
-      accountList: [],
-      admin: '', // 0-无权限 1-权限
-      history_flag: '' //历史数据查看权限，1仅管理员可看，2所有用户可看
-    };
-  },
-  onLoad(option) {
-    this.deviceId = option.deviceId;
-  },
-  onShow() {
-    this.paramList();
-    this.setIntervalObj = setInterval(() => {
-      this.paramList();
-    }, 3000);
-    this.deviceDetail();
-  },
-  onHide() {
-    clearInterval(this.setIntervalObj);
-    this.setIntervalObj = null;
-  },
-  onUnload() {
-    clearInterval(this.setIntervalObj);
-    this.setIntervalObj = null;
-  },
-  computed: {
-    type() {
-      switch (this.dataQuality) {
-        case '差':
-          return 'warning';
-        case '良好':
-          return 'success';
-      }
-    }
-  },
-  methods: {
-    goHistoryData() {
-      uni.navigateTo({
-        url: `./historicalData?deviceId=${this.deviceId}`
-      });
-    },
-    managPersonnel() {
-      uni.navigateTo({
-        url: `./personnelManagement?deviceId=${this.deviceId}`
-      });
-    },
-    async deviceDetail() {
-      this.httpStatus = true;
-      const res = await deviceDetail(this.deviceId);
-	  this.httpStatus = false;
-      this.admin = res.result.admin;
-      this.accountList = res.result.accountList;
-      this.history_flag = res.result.history_flag;
-	  this.deviceName = res.result.detail;
-    },
-    update() {
-      this.httpStatus = true;
-      this.paramList();
-    },
-    async paramList() {
-      const res = await paramList(this.deviceId);
-      this.dataList = res.result.paramDataList.map(item => {
-        return {
-          ...item,
-          param_value: `${item.param_value}${item.unit}`
-        };
-      });
-      this.dataQuality = res.result.quality;
-      this.httpStatus = false;
-    }
-  }
-};
+				deviceName: '',
+				accountList: [],
+				admin: '', // 0-无权限 1-权限
+				history_flag: '', //历史数据查看权限，1仅管理员可看，2所有用户可看
+				modifyShow: false,
+				modeList: []  //模板数据
+			};
+		},
+		onLoad(option) {
+			this.deviceId = option.deviceId;
+		},
+		onShow() {
+			this.paramList();
+			this.setIntervalObj = setInterval(() => {
+				this.paramList();
+			}, 3000);
+			this.deviceDetail();
+		},
+		onHide() {
+			clearInterval(this.setIntervalObj);
+			this.setIntervalObj = null;
+		},
+		onUnload() {
+			clearInterval(this.setIntervalObj);
+			this.setIntervalObj = null;
+		},
+		computed: {
+			type() {
+				switch (this.dataQuality) {
+					case '差':
+						return 'warning';
+					case '良好':
+						return 'success';
+				}
+			}
+		},
+		methods: {
+			async openModify(index) {
+				if(this.dataList[index].modify_flag == 0){
+					console.log("可以编辑");
+					const res = await modeDetail(this.dataList[index].model);
+					this.modifyShow = true;
+				}
+			},
+			actionClick(index) {
+				
+			},
+			goHistoryData() {
+				uni.navigateTo({
+					url: `./historicalData?deviceId=${this.deviceId}`
+				});
+			},
+			managPersonnel() {
+				uni.navigateTo({
+					url: `./personnelManagement?deviceId=${this.deviceId}`
+				});
+			},
+			async deviceDetail() {
+				this.httpStatus = true;
+				const res = await deviceDetail(this.deviceId);
+				this.httpStatus = false;
+				this.admin = res.result.admin;
+				this.accountList = res.result.accountList;
+				this.history_flag = res.result.history_flag;
+				this.deviceName = res.result.detail;
+			},
+			update() {
+				this.httpStatus = true;
+				this.paramList();
+			},
+			async paramList() {
+				const res = await paramList(this.deviceId);
+				this.dataList = res.result.paramDataList.map(item => {
+					return {
+						...item,
+						param_value: `${item.param_value}${item.unit}`
+					};
+				});
+				this.dataQuality = res.result.quality;
+				this.httpStatus = false;
+			}
+		}
+	};
 </script>
 
 <style scoped lang="less">
-.main {
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  .data-content {
-    flex: 1;
-  }
-  .btn-content {
-    padding: 0 20px;
-  }
-  .title {
-    display: flex;
-    align-items: center;
-    flex-direction: row-reverse;
-  }
-  /deep/.u-cell__value {
-    // font-size: 14px;
-    display: flex;
-    align-items: center;
-    flex-direction: row-reverse;
-  }
-}
+	.main {
+		min-height: 100vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+
+		.data-content {
+			flex: 1;
+		}
+
+		.btn-content {
+			padding: 0 20px;
+		}
+
+		.title {
+			display: flex;
+			align-items: center;
+			flex-direction: row-reverse;
+		}
+
+		/deep/.u-cell__value {
+			// font-size: 14px;
+			display: flex;
+			align-items: center;
+			flex-direction: row-reverse;
+		}
+
+		/deep/.u-cell__right-icon-wrap {
+			margin-top: 20upx;
+		}
+	}
 </style>
